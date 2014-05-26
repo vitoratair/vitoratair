e6099a87615402d49be3c3a4d7c56eaf698ab9ca
Vitor Atair <vitor.ruts@gmail.com>
2014-05-26 15:26:20 -0300

    inicializando repositório

---
+++ README.md
@@ -0,0 +1,5 @@
+vitoratair
+==========
+
+how to for many things
+
---
+++ howto/core/views.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+from django.shortcuts import render
+
+def home(request):
+    """ 
+    	Esse método tem por objetivo exibir o template da página inicial
+    """
+
+    return render(request, 'index.html')
