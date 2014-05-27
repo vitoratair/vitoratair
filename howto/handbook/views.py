8a5f31d45f53ddb9ae7950d7b209bf3c7177c8b9
Vitor Atair <vitor.ruts@gmail.com>
2014-05-27 15:33:43 -0300

    added a new subject about "How python was started"

--- howto/handbook/views.py
+++ howto/handbook/views.py
@@ -8,3 +8,10 @@ def python(request):
     """
 
     return render(request, 'python/index.html')	
+
+def pythonStart(request):
+    """ 
+    	Esse método tem por objetivo exibir o template da página "O começo de tudo" sobre python
+    """
+
+    return render(request, 'python/start.html')    
--- howto/urls.py
+++ howto/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^pythonStart/', 'howto.handbook.views.pythonStart', name='python'),
     url(r'^python/', 'howto.handbook.views.python', name='python'),
     url(r'^$', 'howto.core.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
