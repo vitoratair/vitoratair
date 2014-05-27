bf063e1bc4deba56af53da4e538e1a3c1ebe0df8
Vitor Atair <vitor.ruts@gmail.com>
2014-05-27 15:52:22 -0300

    added test file to handbook app

--- howto/handbook/tests.py
+++ howto/handbook/tests.py
@@ -1,3 +1,42 @@
+# -*- coding: utf-8 -*-
+
 from django.test import TestCase
 
 # Create your tests here.
+class CoreTest(TestCase):
+
+    """
+        Classe responsável pelos métodos de testes referente a app handbook
+    """
+
+    def test_get_python(self):
+        """
+        GET / must return  status code 200
+        """
+
+        response = self.client.get('/python/')
+        self.assertEqual(200, response.status_code)
+
+    def test_template_python(self):
+        """
+        Template / must return  'python/index.html'
+        """
+
+        response = self.client.get('/python/')
+        self.assertTemplateUsed(response, 'python/index.html')
+
+    def test_get_pythonStart(self):
+        """
+        GET / must return  status code 200
+        """
+
+        response = self.client.get('/pythonStart/')
+        self.assertEqual(200, response.status_code)
+
+    def test_template_pythonStart(self):
+        """
+        Template / must return  'python/index.html'
+        """
+
+        response = self.client.get('/pythonStart/')
+        self.assertTemplateUsed(response, 'python/start.html')        
