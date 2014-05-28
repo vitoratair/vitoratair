ebb25f21274c96fd6796b1a4b0445cd0dc0781cc
Vitor Atair <vitor.ruts@gmail.com>
2014-05-28 16:04:16 -0300

    changed the SECRET_KEY to try travis work

--- howto/settings.py
+++ howto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).parent
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'fi4rx*k=y=sq+2)+jy)w&)somlx+^kih^uu*3osf!^$h!41q9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
