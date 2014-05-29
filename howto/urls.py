91d9491225f8fef3bc9c3adb547393668ac15eed
Vitor Atair <vitor.ruts@gmail.com>
2014-05-28 22:04:15 -0300

    refactoring - added url patterns to the core handbook

--- howto/urls.py
+++ howto/urls.py
@@ -4,12 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'', include('howto.core.urls', namespace='core')),    
-    url(r'^linux/', 'howto.handbook.views.linux', name='linux'),
-    url(r'^pythonOO/', 'howto.handbook.views.pythonOO', name='python'),    
-    url(r'^pythonData/', 'howto.handbook.views.pythonData', name='python'),
-    url(r'^pythonFirst/', 'howto.handbook.views.pythonFirst', name='python'),
-    url(r'^pythonStart/', 'howto.handbook.views.pythonStart', name='python'),
-    url(r'^python/', 'howto.handbook.views.python', name='python'),    
+	url(r'', include('howto.core.urls', namespace='core')),
+	url(r'^handbook/', include('howto.handbook.urls', namespace='handbook')),       
     url(r'^admin/', include(admin.site.urls)),
 )
